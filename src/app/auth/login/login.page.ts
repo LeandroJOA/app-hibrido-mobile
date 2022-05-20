25d4db664770c5884ca24e90616b9e6fb61fa3ad
C1320996 Joao Victor Lopo do Nascimento Alves Pereira <c1320996@interno.bb.com.br>
2022-05-20 18:48:26 -0300

    Mock de contas validas

--- src/app/auth/login/login.page.ts
+++ src/app/auth/login/login.page.ts
@@ -10,6 +10,15 @@ export class LoginPage implements OnInit {
 
   loginForm: FormGroup;
 
+  name: string;
+  password: string;
+
+  usuarios: any = [
+    {email: 'bcf', senha: 'bcf'},
+    {email: 'mamae', senha: 'abacaxi'},
+    {email: 'papai', senha: 'melancia'},
+  ];
+
   constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
@@ -23,12 +32,33 @@ export class LoginPage implements OnInit {
     });
   }
 
-  get email():FormControl {
+  get email(): FormControl {
     return <FormControl>this.loginForm.get('email');
   }
 
-  get senha():FormControl {
+  get senha(): FormControl {
     return <FormControl>this.loginForm.get('senha');
+  }
+
+  realizarLogin(){
+    this.usuarios.array.forEach(us => {
+      us = this.usuarios;
+      if (us.email === this.name && us.senha === this.password) {
+        const isLoggedIn = false;
+        const router = document.querySelector('ion-router');
+        const routeRedirect = document.createElement('ion-route-redirect');
+        routeRedirect.setAttribute('from', '*');
+        routeRedirect.setAttribute('to', '/register');
+
+        if (!isLoggedIn) {
+          router.appendChild(routeRedirect);
+        }
+          return true;
+      }
+    });
+
+    alert('Dados incorretos, tente novamente.');
+    return false;
   }
 
 }
