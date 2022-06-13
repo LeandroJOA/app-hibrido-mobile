d631624b93ebb4f22bdfc229501c92082795b286
Leandro Alves <leandrojoapi@gmail.com>
2022-06-13 00:22:48 -0300

    merge

--- backend/index.js
+++ backend/index.js
@@ -7,12 +7,9 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 
 const usersRouter = require('./routes/users');
-<<<<<<< HEAD
 const coursesRouter = require('./routes/courses');
 const classesRouter = require('./routes/classes');
-=======
 const studentsRouter = require('./routes/students');
->>>>>>> 0c53846c1bf0366271ff688d952d0e39bc26e6a4
 
 const app = express();
 
@@ -28,11 +25,8 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 app.use('/users', usersRouter);
-<<<<<<< HEAD
 app.use('/courses', coursesRouter);
 app.use('/classes', classesRouter);
-=======
 app.use('/students', studentsRouter);
->>>>>>> 0c53846c1bf0366271ff688d952d0e39bc26e6a4
 
 module.exports = app;
